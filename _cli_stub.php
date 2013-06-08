2b76d6c0cd8f5819d970750cb1538b9aa3d6e71d
Alan Storm <astorm@alanstorm.com>
2013-06-08 16:51:32 -0700

    We are not an application.

--- _cli_stub.php
+++ _cli_stub.php
@@ -4,7 +4,7 @@
 Phar::mapPhar('magento-tar-to-connect.phar');
 
 $application = require_once 'phar://magento-tar-to-connect.phar/magento-tar-to-connect.php';
-$application->setPharMode(true);
-$application->run();
+// $application->setPharMode(true);
+// $application->run();
 
 __HALT_COMPILER();
