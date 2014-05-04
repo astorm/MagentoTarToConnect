04f52181a0d97c9faddfdce30cdf91f6dfcfc624
Ashley Schroder <ashley.schroder@gmail.com>
2014-05-04 21:11:32 +1200

    Super useful feature missing from example

--- example-config.php
+++ example-config.php
@@ -17,6 +17,10 @@ return array(
 'extension_version'      => '1.0.3',
 'skip_version_compare'   => false,
 
+//You can also have the package script use the version in the module you 
+//are packaging with. 
+'auto_detect_version'   => false,
+
 //Where on your local system you'd like to build the files to
 'path_output'            => '/fakehome/Pulsestorm/var/build-connect',
 
